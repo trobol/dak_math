730b59c5820092079b189b6fd39a36e05348e0e2
thorfernbacher <thorfernbacher@gmail.com>
2020-02-11 13:50:57 -0500

    init commit

---
+++ include/vec2.h
@@ -0,0 +1,8 @@
+#ifndef _DAK_VECTOR_LIB_VEC2
+#define _DAK_VECTOR_LIB_VEC2
+
+struct vec2
+{
+};
+
+#endif
---
+++ include/vec4.h
@@ -0,0 +1,8 @@
+#ifndef _DAK_VECTOR_LIB_VEC4
+#define _DAK_VECTOR_LIB_VEC4
+
+struct vec4
+{
+};
+
+#endif
