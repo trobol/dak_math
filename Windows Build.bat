6800d2c57c3587ba68b5f99e5d9d343b4569a392
Alexnader Watermen <AlexanderWatermenAWC@gmail.com>
2021-03-19 01:51:39 -0400

    VS-2013 > support

--- Windows Build.bat
+++ Windows Build.bat
@@ -3,8 +3,9 @@
 SETLOCAL EnableDelayedExpansion
 set NL=^
 
-REM randomly breaks
+
 cd /d %~dp0
+REM randomly breaks
 
 REM set output file
 set output=dak_math.h 
@@ -40,5 +41,69 @@ type .\src\operators.h >> %output%
 
 type .\src\types.h >> %output%
 
-echo tail >> %output%
+echo %tail% >> %output%
 
+set installed = 0
+
+reg query "HKEY_CLASSES_ROOT\VisualStudio.DTE.16.0" >> nul 2>&1
+if %ERRORLEVEL% NEQ 0 ( 
+	
+	echo VS 2019 not installed 
+
+	reg query "HKEY_CLASSES_ROOT\VisualStudio.DTE.15.0" >> nul 2>&1 
+	if %ERRORLEVEL% NEQ 0 ( 
+
+		echo VS 2017 not installed 
+
+		reg query "HKEY_CLASSES_ROOT\VisualStudio.DTE.14.0" >> nul 2>&1
+		if %ERRORLEVEL% NEQ 0 ( 
+	
+			echo VS 2015 not installed 
+
+			reg query "HKEY_CLASSES_ROOT\VisualStudio.DTE.12.0" >> nul 2>&1 
+			if %ERRORLEVEL% NEQ 0 ( 
+
+				echo Please install VisualStudio 2013 or higher
+				pause
+
+			) else ( 
+
+				echo VS 2013 installed. 
+				call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
+				set installed=1
+			) 
+
+		) else ( 
+
+			echo VS 2015 installed.
+			call "C:\Program Files (x86)\Microsoft Visual Studio 15.0\VC\vcvarsall.bat" x86_amd64
+			set installed=1
+		) 
+	) else ( 
+
+		echo VS 2017 installed. 
+		echo missing path
+		echo if you find it please put it here
+		echo https://github.com/trobol/dak_math
+		REM call "C:\Program Files (x86)\Microsoft Visual Studio 15.0\VC\vcvarsall.bat" x86_amd64
+		set installed=1
+
+	) 
+
+) else ( 
+
+	echo VS 2019 installed.
+	call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+	set installed=1
+
+) 
+
+
+if %installed%==1 (
+pause
+cd /d %~dp0
+pause
+cl /Tp %output% test\test.cpp 
+pause
+
+)
